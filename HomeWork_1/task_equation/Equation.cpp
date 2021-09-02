1cd203edc5568c1f0838d677f1caa3796abc4f39
Veji <45602069+Vedji@users.noreply.github.com>
2021-09-02 20:56:06 +0300

    Create Equation.cpp

---
+++ HomeWork_1/task_equation/Equation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    double b, c;
+    cout << "Enter number b: ";
+    cin >> b;
+    cout << "Enter number c: ";
+    cin >> c;
+    if (b != 0) {
+        cout << "x = " << -(c / b);
+    }
+    else {
+        cout << "Encorect c." << endl;
+    }
+    system("pause");
+}
+© 2021 GitHub, Inc.
