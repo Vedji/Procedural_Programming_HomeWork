e7af7ba68b0537869a90075254761fa275aa11a5
Veji <45602069+Vedji@users.noreply.github.com>
2021-09-03 11:57:41 +0300

    Create tabulation.cpp

---
+++ HomeWork_2/task_tabulation/tabulation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+    setlocale(0, "");
+    float x = -4;
+    while(x <= 4.){
+        cout << "y(" << x << ") = " <<  (pow(x, 2) - 2 * x + 2) / (x - 1) << endl;
+        x += 0.5;
+    }
+    system("Pause");
+    return 0;
+}
