f97683993c8347f45679d623db9a0912e86a3054
Veji <45602069+Vedji@users.noreply.github.com>
2021-09-05 19:55:22 +0300

    Update tabulation.cpp

--- HomeWork_2/task_tabulation/tabulation.cpp
+++ HomeWork_2/task_tabulation/tabulation.cpp
@@ -3,13 +3,24 @@
 #include <cmath>
 using namespace std;
 
+double month_payments(double S, double p, double n){
+    double r = p / 100.;
+    if((12 * (pow(1 + r, n) - 1)) == 0){
+        return 0;
+    }
+    return (S * r * pow(1 + r, n)) / (12 * (pow(1 + r, n) - 1));
+}
 
 int main() {
     setlocale(0, "");
     float x = -4;
     while(x <= 4.){
-        cout << "y(" << x << ") = " <<  (pow(x, 2) - 2 * x + 2) / (x - 1) << endl;
-        x += 0.5;
+        if(x - 1 != 1) {
+            cout << "y(" << x << ") = " << (pow(x, 2) - 2 * x + 2) / (x - 1) << endl;
+            x += 0.5;
+        }else{
+            cout << "y(1) - Zero" << endl;
+        }
     }
     system("Pause");
     return 0;
