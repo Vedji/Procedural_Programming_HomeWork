417fca683b7540df37006f179ee352e8e688c33f
Veji <45602069+Vedji@users.noreply.github.com>
2021-09-08 23:31:08 +0300

    Update truncated_cone_problem.cpp

--- HomeWork_2/task_cone_problem/truncated_cone_problem.cpp
+++ HomeWork_2/task_cone_problem/truncated_cone_problem.cpp
@@ -1,12 +1,19 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
 using namespace std;
 
+/*
+Enter h: 4
+Enter r: 4
+Enter R: 4
+Enter l: 5
+*/
+
 int main() {
-    setlocale(0, "rus");
+    setlocale(0, "");
     double h, R, r, l;
-    const double Pi = 3.1415926535;
     cout << "Enter h: ";
     cin >> h;
     cout << "Enter r: ";
@@ -15,8 +22,32 @@ int main() {
     cin >> R;
     cout << "Enter l: ";
     cin >> l;
-    cout << "V = " << (1./3.) * Pi * h * (pow(R, 2) + R * r + pow(R, 2)) << endl;
-    cout << "S = " << Pi * (pow(R, 2) + (R + r) * l + pow(r, 2)) << endl;
+    if (sqrt(pow(h, 2) + pow((R - r), 2)) !=  l) {
+        cout << "Конус не существует" << endl;
+        system("Pause");
+        return 0;
+    }
+    if (r >= R) {
+        if (r == R && h == l) {
+            cout << "V цилиндра = " << (M_PI * pow(r, 2) * h) << endl;
+            cout << "S цилиндра = " << (2 * M_PI * pow(r, 2) + 2 * M_PI * r * h) << endl;
+        } 
+        else {
+            cout << "Это чото не известное науки." << endl;
+        }
+        system("Pause");
+        return 0;
+    }
+    if (h < 0 || R < 0 || r < 0 || l < 0) {
+        cout << "Отрицательные величины не принимаем!" << endl;
+        system("Pause");
+        return 0;
+    }
+    else {
+        cout << "V = " << (1. / 3.) * M_PI * h * (pow(R, 2) + R * r + pow(R, 2)) << endl;
+    }
+
+    cout << "S = " << M_PI * (pow(R, 2) + (R + r) * l + pow(r, 2)) << endl;
     system("Pause");
     return 0;
 }
