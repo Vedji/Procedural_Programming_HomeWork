3e7707bca980fbe1ee4abe17e28bea3f999d7acf
Veji <45602069+Vedji@users.noreply.github.com>
2021-09-15 17:35:37 +0300

    Update finding_loan_interest.cpp
    Great

--- HomeWork_3/task_finding_loan_interest/finding_loan_interest.cpp
+++ HomeWork_3/task_finding_loan_interest/finding_loan_interest.cpp
@@ -2,60 +2,43 @@
 #include <cmath>
 using namespace std;
 
-double month_payments(double S, double p, double n) {
-    double r = p / 100.;
-    if ((12 * (pow(1 + r, n) - 1)) == 0 || (((int)n % 2) == 0 && (1 + r) < 0)) {
-
-        return -1;
-    }
-    return (S * r * pow(1 + r, n)) / (12 * (pow(1 + r, n) - 1));
+double loan_find(double s, double p, double n) {
+	double r = p / 100;
+	if ((12 * (pow((1 + r), n) - 1)) == 0) { return 0; }
+	return ((s * r * pow((1 + r), n)) / (12 * (pow((1 + r), n) - 1)));
 }
 
-int main()
-{
-    setlocale(0, "");
+int main() {
+	double s, p, n, m, step = 10;
+	s = 100000;
+	n = 1;
+	m = 9166.66;
+	bool flag = true;
+	while (flag) {
+		// cout << "[DEBUG]: p = " << step << ", m = " << loan_find(s, step, n)  << endl;
+		if (loan_find(s, step, n) < m) {
+			step = step + step / 2.;
+		}
+		else if (loan_find(s, step, n) > m) {
+			step = step - step / 2.;
+		}
 
-    double S, M, N, s, m, n, r;
-    bool flag = true;
-
-    cout << "Введите S: ";
-    cin >> S;
-    cout << "Введите m: ";
-    cin >> M;
-    cout << "Введите n: ";
-    cin >> N;
-    if (S <= 0 || M <= 0 || N <= 0) {
-        cout << " Такие числа не принимаем. " << endl;
-        return 0;
-    }
-    r = S;
-    m = month_payments(S, r, N);
-
-    while (flag) {
-        // system("CLS");
-        if (_isnan(m) || _isnan(r)) {
-            cout << "Error" << endl;
-            break;
-        }
-        if ((M - 0.01) < m && m < (M + 0.01)) {
-            cout << "p = " << r << ", m = " << m << ", M = " << M << endl;
-            break;
-        }
-        else {
-            if (m < M && r - r / 2 != 0) r += (r - r / 2) / 2;
-            if (m > M && r * 2 - r != 0) r -= (r * 2 - r) / 2;
-        }
-        m = month_payments(S, r, N);
-        cout << "[DEBUG]: p = " << r << ", m = " << m << endl;
-        if (m)
-            if (0 >= r || r > 100000 || m > 1000000000000000000) break;
-        if ((M - 0.001) < m < (M + 0.001)) flag = false;
-    }
-    if (!flag) {
-        cout << "[RELEASE]: p = " << r << ", m = " << m << endl;
-    }
-    else {
-        cout << "Error" << endl;
-    }
-    system("Pause");
+		if ((m - 0.005) < loan_find(s, step, n) && loan_find(s, step, n) < (m + 0.006)) {
+			flag = false;
+			for (double i = (step - 0.005); i < (step + 0.006); i += 0.0000001) {
+				if (((m - 0.000001) <= loan_find(s, i, n) && loan_find(s, i, n) <= (m + 0.000001)) || loan_find(s, i, n) == m) {
+					step = i;
+					break;
+				}
+			}
+		}
+		
+	}
+	if (!flag) {
+		cout << "[RELEASE]: p = " << step << ", m = " << loan_find(s, step, n);
+	}
+	else {
+		cout << "[RELEASE]: Error";
+	}
+	return 0;
 }
