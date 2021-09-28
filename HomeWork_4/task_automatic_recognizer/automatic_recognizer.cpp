647b510a210c2bd2188454c498c21acd2e17e322
Veji <45602069+Vedji@users.noreply.github.com>
2021-09-29 02:02:07 +0300

    Create automatic_recognizer.cpp

---
+++ HomeWork_4/task_automatic_recognizer/automatic_recognizer.cpp
@@ -0,0 +1,53 @@
+// Задача "Автоматический распознаватель"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int symbol_to_int(char symbol) {
+	switch (symbol){
+	case 'I': return    1; break;
+	case 'V': return    5; break;
+	case 'X': return   10; break;
+	case 'L': return   50; break;
+	case 'C': return  100; break;
+	case 'D': return  500; break;
+	case 'M': return 1000; break;
+	}
+	return 0;
+}
+
+int main()
+{
+	setlocale(0, "");
+	string line = "";
+	getline(cin, line);
+	if (line.size() <= 0) { cout << "Error" << endl; return 0; }
+	for (int i = 0; i < line.size(); i++) if (symbol_to_int(line[i]) == 0) return 0;
+	int number = 0;
+	int last = symbol_to_int(line[0]);
+	int counter = 1;
+	for (int i = 1; i < line.size(); i++) {
+		if (counter > 3) {
+			cout << "Число не соответствует римской классичсекой записи." << endl;
+			return 0;
+		}
+		cout << "Number = " << number << endl;
+		int cur = symbol_to_int(line[i]);
+		if (cur == last) counter++;
+
+		if (cur > last) { 
+				number -= last * counter;
+		}
+		if (cur < last) number += last * counter;
+		if (last != cur) counter = 1;
+		last = cur;
+
+	}
+	// int cur = symbol_to_int(line[line.size() - 1]);
+	// if (cur > last) number -= ++last * counter;
+	// if (cur <= last) number += ++last * counter;
+	if (last == symbol_to_int(line[line.size() - 1])) number += symbol_to_int(line[line.size() - 1]) * counter;
+	
+	cout << "Number = " << number << endl;
+}
